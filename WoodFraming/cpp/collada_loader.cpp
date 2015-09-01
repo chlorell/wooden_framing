4497143025b858e06549aa77ee5ce914a20c1fcc
Mateusz Bugaj <chlorell@MacBook-Pro-Mateusz-Bugaj-130.local>
2015-09-01 13:53:51 +0200

    adding collada loader with sample files

---
+++ WoodFraming/cpp/collada_loader.cpp
@@ -0,0 +1,9 @@
+//
+//  collada_loader.cpp
+//  WoodFraming
+//
+//  Created by Mateusz Bugaj on 01.09.2015.
+//  Copyright (c) 2015 Ocean Planet Studios. All rights reserved.
+//
+
+#include "collada_loader.h"
